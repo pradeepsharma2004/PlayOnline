135fbab53324693528d31610ee3b92fa75104feb
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-04-16 19:51:31 +0530

    views of PlayOnline modules is added

---
+++ PlayOnline/views.py
@@ -0,0 +1,10 @@
+from django.http import Http404
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    return render(request, 'PlayOnline/index.html', context=None)
+
+
+
