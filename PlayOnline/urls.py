f78d9efa88d9ff64f6b48a6e1e16e11de3381a2b
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-04-16 19:47:50 +0530

    Urls.py of PlayOnline Modules is added

--- PlayOnline/urls.py
+++ PlayOnline/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^music/', include('music.urls')),
+    url(r'^image/', include('image.urls')),
+    url(r'^video/', include('video.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
