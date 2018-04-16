019c5039fd5f71ff3e6fd9ea251b404e438e3e44
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-04-16 19:39:04 +0530

    apps Configured in settings.py

--- PlayOnline/settings.py
+++ PlayOnline/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'music.apps.MusicConfig',
+    'image.apps.ImageConfig',
+    'video.apps.VideoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
