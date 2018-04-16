96ac055ef1793fd0fec27710106829fb0457c016
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-04-16 22:20:33 +0530

    views.py is added to the video app

--- video/views.py
+++ video/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import Album, Videos
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'video/index.html'
+    context_object_name = 'video_album_list'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    template_name = 'video/Detail.html'
+    model = Album
