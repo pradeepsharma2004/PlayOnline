988dd17389b184df64513e8c8f076b8b373d83ab
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-04-16 22:30:41 +0530

    views.py is added to the image app

--- image/views.py
+++ image/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Album
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'image/index.html'
+    context_object_name = 'image_album_list'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+
+class DetailView(generic.DetailView):
+    template_name = 'image/Detail.html'
+    model = Album
+
