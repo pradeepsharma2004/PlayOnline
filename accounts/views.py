98c00e714b7875bf9db5a68a2d82c8f08a82b3af
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-05-03 22:10:03 +0530

    urls.py and views.py added to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
+
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('playonline:index')
+
+
+
+
+
+
