7bcc1971d8adbae5d68f0b86726b97a95daa2c23
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-05-06 23:41:43 +0530

    static and accounts app configured

--- PlayOnline/settings.py
+++ PlayOnline/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'music.apps.MusicConfig',
     'image.apps.ImageConfig',
     'video.apps.VideoConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'PlayOnline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
