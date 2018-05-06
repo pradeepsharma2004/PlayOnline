99a742deb566019004e878b55f396b6b20ed390e
pradeepsharma2004 <aryanarjunsharma@gmail.com>
2018-05-06 23:48:09 +0530

    views.py in mainapp updated

--- PlayOnline/views.py
+++ PlayOnline/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
-    return render(request, 'PlayOnline/index.html', context=None)
+    return render(request, 'playonline/index.html', context=None)
 
 
 
